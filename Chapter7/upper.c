a17ceb1b6d1049b4f1190872f98df9a9a84d7771
ArunKRamachandran <arunkramachandran92@gmail.com>
2014-11-03 12:02:06 +0530

    updated | on 3/11/14

---
+++ Chapter7/upper.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+main(int argc, char *argv[]) {
+
+	int c;
+
+	printf("Argv[0] :   ");
+	printf("%s\n", argv[0]);
+
+/*	printf("Agrv[1] :   ");
+	printf("%s\n", argv[1]);
+*/
+	if (strcmp(argv[0],"lower") == 0)
+		while ((c = getchar()) != EOF)
+			putchar(tolower(c));
+	if (strcmp(argv[0],"upper") == 0)
+		while ((c = getchar()) != EOF)
+			putchar(toupper(c));
+
+	return 0;
+}
